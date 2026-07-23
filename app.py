b4e75024d0b1f9e9fc0fcc9d4c14af8f83207de4
Aicha Gory <aichagory30@gmail.com>
2026-07-23 17:08:45 -0400

    Try SSL port 465 for Gmail SMTP instead of STARTTLS 587

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ app.secret_key = SECRET_KEY
 
 # Email Configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = MAIL_USERNAME
 app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
 
